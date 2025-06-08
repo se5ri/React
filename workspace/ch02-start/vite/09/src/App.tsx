13178545ef44ccf864b865aeb144fb0e1a0e2bd0
오서현 <bbibbi1118@naver.com>
2025-06-09 00:02:03 +0900

    ✨ 25.06.05

--- workspace/ch02-start/vite/09/src/App.tsx
+++ workspace/ch02-start/vite/09/src/App.tsx
@@ -1,15 +1,26 @@
 import Title from "./components/Title";
 import TodoList from "./pages/todo/List";
 
+export interface TodoItem {
+  _id: number;
+  title: string;
+  done: boolean;
+}
+
 // Title, TodoList의 부모 컴포넌트
 function App() {
+  const list: TodoItem[] = [
+    { _id: 1, title: "자바스크립트 공부", done: true },
+    { _id: 2, title: "JS 프로젝트", done: true },
+    { _id: 3, title: "React 공부", done: false },
+  ];
   return (
     <div id="app">
       <div>
         <Title />
         <Title titleName="타이틀 1" />
         <Title titleName="타이틀 2" />
-        <TodoList />
+        <TodoList list={list} />
       </div>
     </div>
   );
