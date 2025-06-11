bcd0d6a85ec339c5c8dd4b3791d26ab1fd0f148b
오서현 <bbibbi1118@naver.com>
2025-06-11 15:20:07 +0900

    ✨ ch 04 리액트 훅: 01 useState - 상태 관리

--- workspace/ch04-hooks/01-useState/src/App.tsx
+++ workspace/ch04-hooks/01-useState/src/App.tsx
@@ -1,16 +1,29 @@
 // TODO 1. 메세지를 입력하면 입력 메세지에 반영되도록 수정
 
+import { useState } from "react";
+
 function App() {
+  const [message, setMessage] = useState("");
+
+  // const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   setMessage(event.target.value);
+  // };
+  // 서버의 시간을 ahax로 요청
+  const time = 125;
   return (
     <>
       <h1>01 useState - 상태 관리</h1>
       <div>
-        <input type="text" value="hello" />
+        <input
+          type="text"
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+        />
         <br />
-        <span>입력 메세지: hello</span>
+        <span>입력 메세지: {message}</span>
       </div>
     </>
   );
 }
 
-export default App
+export default App;
