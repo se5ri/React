2e50f7920a71f7985486c352d50fd91915cd3228
오서현 <bbibbi1118@naver.com>
2025-06-23 09:42:11 +0900

    ♻️

--- workspace/ch12-app/todolist/06-api/src/pages/TodoAdd.tsx
+++ workspace/ch12-app/todolist/06-api/src/pages/TodoAdd.tsx
@@ -4,59 +4,67 @@ import { useForm } from "react-hook-form";
 import { Link } from "react-router";
 
 function TodoAdd() {
-
   const axiosInstance = useAxiosInstance();
 
-  const { register, handleSubmit, reset, setFocus, formState: { errors } } = useForm<TodoItem>();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    setFocus,
+    formState: { errors },
+  } = useForm<TodoItem>();
 
   const addTodo = async (formData: TodoItem) => {
-    console.log('API 서버에 등록 요청', formData);
-    // API 서버에 등록 요청
-    try{
-      await axiosInstance.post('/todolist', formData);
+    "use no memo";
 
-      alert('할일이 등록 되었습니다.');
+    console.log("API 서버에 등록 요청", formData);
+    // API 서버에 등록 요청
+    try {
+      await axiosInstance.post("/todolist", formData);
+
+      alert("할일이 등록 되었습니다.");
       reset();
-      setFocus('title');
-    }catch(err){
+      setFocus("title");
+    } catch (err) {
       console.error(err);
-      alert('할일 등록에 실패했습니다.');
+      alert("할일 등록에 실패했습니다.");
     }
-    
   };
 
   return (
     <div id="main">
       <h2>할일 추가</h2>
       <div className="todo">
-        <form onSubmit={ handleSubmit(addTodo) }>
+        <form onSubmit={handleSubmit(addTodo)}>
           <label htmlFor="title">제목 :</label>
-          <input 
-            type="text" 
-            id="title" 
+          <input
+            type="text"
+            id="title"
             autoFocus
-            { ...register('title', {
-              required: '제목을 입력하세요.',
+            {...register("title", {
+              required: "제목을 입력하세요.",
               pattern: {
                 value: /\S/,
-                message: '제목에 공백만 입력할 수 없습니다.'
-              }
-            }) }
+                message: "제목에 공백만 입력할 수 없습니다.",
+              },
+            })}
           />
-          <div className="input-error">{ errors.title?.message }</div>
+          <div className="input-error">{errors.title?.message}</div>
           <br />
           <label htmlFor="content">내용 :</label>
-          <textarea 
-            id="content" cols={23} rows={5}
-            { ...register('content', {
-              required: '내용을 입력하세요.',
+          <textarea
+            id="content"
+            cols={23}
+            rows={5}
+            {...register("content", {
+              required: "내용을 입력하세요.",
               pattern: {
                 value: /\S/,
-                message: '내용에 공백만 입력할 수 없습니다.'
-              }
-            }) }
+                message: "내용에 공백만 입력할 수 없습니다.",
+              },
+            })}
           ></textarea>
-          <div className="input-error">{ errors.content?.message }</div>
+          <div className="input-error">{errors.content?.message}</div>
           <br />
           <button type="submit">추가</button>
           <Link to="/list">취소</Link>
--- workspace/ch12-app/todolist/06-api/vite.config.ts
+++ workspace/ch12-app/todolist/06-api/vite.config.ts
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: [
-          ["babel-plugin-react-compiler", {}]
-        ]
-      }
-    })
+    react(),
+    // react({
+    //   babel: {
+    //     plugins: [
+    //       ["babel-plugin-react-compiler", {}]
+    //     ]
+    //   }
+    // })
   ],
   resolve: {
     alias: [
@@ -20,5 +21,4 @@ export default defineConfig({
       { find: "@hooks", replacement: "/src/hooks" },
     ],
   },
-  
-})
+});
