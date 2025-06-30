432f7c78d618f37ba36408e1e52f6d43e1d900f9
오서현 <bbibbi1118@naver.com>
2025-06-30 21:05:04 +0900

    ♻️

--- workspace/ch10-nextjs/02-cna/src/app/about/page.tsx
+++ workspace/ch10-nextjs/02-cna/src/app/about/page.tsx
@@ -2,7 +2,7 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "About",
-  description: "About 페이지입니다.",
+  description: "About 페이지입니다.", // 검섹엔진 최적화를 위해 사용
 };
 
 export default function AboutPage() {
