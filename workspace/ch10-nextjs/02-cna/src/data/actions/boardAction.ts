9438f16fc62e68d5377b95af8425e9c650cc4ffa
오서현 <bbibbi1118@naver.com>
2025-07-03 22:05:54 +0900

    ♻️

--- workspace/ch10-nextjs/02-cna/src/data/actions/boardAction.ts
+++ workspace/ch10-nextjs/02-cna/src/data/actions/boardAction.ts
@@ -1,22 +1,33 @@
-'use server';
+"use server";
 import { PostInfoRes } from "@/types/board";
+import { revalidateTag } from "next/cache";
 
- // 서버 액션
+import { redirect } from "next/navigation";
+
+// 서버 액션
 
 // 게시글 등록
-export async function createPost(prevState: PostInfoRes, formData: FormData){
-  const title = formData.get('title');
-  const content = formData.get('content');
-  const body = {title, content, type: 'qna'};
+export async function createPost(prevState: PostInfoRes, formData: FormData) {
+  const title = formData.get("title");
+  const content = formData.get("content");
+  const body = { title, content, type: "qna" };
   console.log(body);
   const res = await fetch(`https://fesp-api.koyeb.app/market/posts`, {
-    method: 'POST',
-    body: JSON.stringify({title, content, type: 'qna'}),
+    method: "POST",
+    body: JSON.stringify({ title, content, type: "qna" }),
     headers: {
-      'Content-Type': 'application/json',
-      'Client-Id': 'openmarket'
-    }
+      "Content-Type": "application/json",
+      "Client-Id": "openmarket",
+    },
   });
   const data = await res.json();
+  // 성공적으로 등록되었을 경우
+  if (data.ok) {
+    // '/posts' 페이지에 대한 캐시를 무효화 (최신 게시글 목록 반영을 위해)
+    // revalidatePath("/posts");
+    // revalidateTag("list");
+    // '/posts' 페이지로 이동
+    redirect("/posts");
+  }
   return data;
 }
--- workspace/ch10-nextjs/02-cna/src/data/functions/boardFetch.ts
+++ workspace/ch10-nextjs/02-cna/src/data/functions/boardFetch.ts
@@ -1,4 +1,4 @@
-'use server'; // 서버 함수
+"use server"; // 서버 함수
 
 import { Post } from "@/types/board";
 
@@ -6,10 +6,16 @@ export async function fetchPosts(): Promise<Post[]> {
   // 준비된 API 서버 호출
   const res = await fetch(`https://fesp-api.koyeb.app/market/posts?type=qna`, {
     headers: {
-      'Client-Id': 'openmarket'
-    }
+      "Client-Id": "openmarket",
+    },
+    next: {
+      tags: ["list"],
+      revalidate: 10, // 10초 후에 자동으로 revalidate
+    },
+    //cache: "no-cache", // next 15 기본 옵션
+    cache: "force-cache", // next 14 기본 옵션
   });
   const data = await res.json();
-  console.log('boardFetch', data.item.length);
+  console.log("boardFetch", data.item.length);
   return data.item;
 }
